d276e1eedfbc5a1975dda3cf4a6fd34def819ad1
Ben Klaas <ben@benklaas.com>
2011-07-15 14:30:34 -0500

    remove debug

--- lib/blitz/command/curl.rb
+++ lib/blitz/command/curl.rb
@@ -319,7 +319,6 @@ class Curl < Command # :nodoc:
                     parameter_hash['type'] = 'number'
                     parameter_hash['min'] = $2.to_i
                     parameter_hash['max'] = $3.to_i
-                    p $4
                     parameter_hash['count'] = $5 ? $5.to_i : 1000
                 elsif variable_parameter.match(/^(u|udid)$/)
                     parameter_hash['type'] = 'udid'
